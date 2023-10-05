a289c511030d1b1a2cf73554524dbc538011116c
Wojciech Holisz <golden.gecko@holisz.pl>
2023-10-06 00:02:09 +0200

    Add tiles

---
+++ osm/download.bat
@@ -0,0 +1,1 @@
+curl -o poland-latest.osm.pbf https://download.geofabrik.de/europe/poland-latest.osm.pbf
---
+++ overpass/download.bat
@@ -0,0 +1,2 @@
+curl -o poland-latest.osm.bz2 https://download.geofabrik.de/europe/poland-latest.osm.bz2
+curl -o malopolskie-latest.osm.bz2 https://download.geofabrik.de/europe/poland/malopolskie-latest.osm.bz2
